d8a2d5f28b027869206da92a9da60f91ed6cd146
BlackRing8 <gprakoso123@gmail.com>
2024-02-23 17:31:07 +0700

    migrasi db and fix bug

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex h-full w-full justify-end items-center">{children}</div>;
+  return <div className="flex h-full w-full bg-sky-500 justify-end items-center sm:pr-24">{children}</div>;
 };
 
 export default AuthLayout;
--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,26 +2,25 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
-export async function POST(req: Request,) {
-    try {
-        const { userId } = auth();
-        const { title } = await req.json();
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
 
-        if (!userId) {
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
-
-        const course = await db.course.create({
-            data: {
-                userId,
-                title,
-            }
-        });
-
-        return NextResponse.json(course);
-
-    } catch (error) {
-        console.log("[COURSES]", error);
-        return new NextResponse("internal Error", { status: 500 });
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("internal Error", { status: 500 });
+  }
 }
